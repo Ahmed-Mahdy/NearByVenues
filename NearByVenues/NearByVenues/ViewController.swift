1cf69420c95a5b14f8c9a81642868869e9668779
Mahdy <ahmed.mahdy.abdelrahman@gmail.com>
2022-07-06 14:22:10 +0200

    initial commit

---
+++ NearByVenues/NearByVenues/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NearByVenues
+//
+//  Created by Ahmed Mahdy on 06/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
